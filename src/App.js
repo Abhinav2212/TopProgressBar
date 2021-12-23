c059faf6f6d61ecef2e04ca083c0d0aa3fa60020
ABHINAV MUKHERJEE <91337729+Abhinav2212@users.noreply.github.com>
2021-12-23 12:47:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+import React, { useState } from "react";
+import LoadingBar from "react-top-loading-bar";
+
+const App = () => {
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div>
+      <LoadingBar
+        color="#f11946"
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+        height={4}
+      />
+      <button onClick={() => setProgress(progress + 10)}>Add 10%</button>
+      <button onClick={() => setProgress(progress + 20)}>Add 20%</button>
+      <button onClick={() => setProgress(100)}>Complete</button>
+      <br />
+    </div>
+  );
+};
+
+export default App;
